b7db8dc0a4fabe67453a456bfe90b9d39bf85461
Matt Benton <matt@mattbenton.net>
2014-03-18 13:25:54 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,49 @@
-gulp-aws-s3
-===========
+# gulp-aws-s3
 
-A plugin for Gulp that uploads files to Amazon's S3 service
+A plugin for [Gulp](https://github.com/gulpjs/gulp) that uploads files to Amazon's S3 service.
+
+## Installation
+
+Install `gulp-aws-s3` as a development dependency:
+
+```bash
+npm install --save-dev git://github.com/mattbenton/gulp-aws-s3.git
+```
+
+## AWS Credentials
+
+Create a JSON file with your AWS credentials and bucket name:
+
+```json
+{
+  "key": "ABC123DEF456HIJ789KL",
+  "secret": "abCdEFg1IJk2MnoPqrsTUvWxyZ3+4ABC5D6eFGHi",
+  "bucket": "my-bucket"
+}
+```
+
+## Usage
+
+```js
+var s3 = require('gulp-aws-s3');
+
+gulp.task('deploy', function () {
+  var aws = JSON.parse(fs.readFileSync('aws.json'));
+
+  var options = {
+    'headers': {
+      'Cache-Control': 'max-age=315360000, no-transform, public',
+      'Content-Type': 'application/javascript',
+      'Content-Encoding': 'gzip'
+    },
+    'uploadPath': 'some/subdirectory/'
+  };
+
+  return gulp.src('./dist/*.js')
+    .pipe(gzip())
+    .pipe(rename('platform.js'))
+    .pipe(s3(aws, options));
+});
+```
+
+
---
+++ index.js
@@ -0,0 +1,65 @@
+// through2 is a thin wrapper around node transform streams
+var through = require('through2');
+var gutil = require('gulp-util');
+var knox = require('knox');
+var PluginError = gutil.PluginError;
+
+// Consts
+const PLUGIN_NAME = 'gulp-aws-s3';
+
+// Plugin level function(dealing with files)
+function gulpAwsS3 ( aws, options ) {
+  if ( !aws ) {
+    throw PluginError(PLUGIN_NAME, 'Missing AWS credentials!');
+  }
+
+  var client = knox.createClient(aws);
+
+  var headers = {
+    'x-amz-acl': 'public-read'
+  };
+
+  if ( options.headers ) {
+    for ( var key in options.headers ) {
+      headers[key] = options.headers[key];
+    }
+  }
+
+  // Creating a stream through which each file will pass
+  var stream = through.obj(function(file, enc, callback) {
+    if ( file.isNull() ) {
+      // Do nothing if no contents
+    }
+
+    var uploadPath = file.path.replace(file.base, options.uploadPath || '');
+
+    var method = null;
+    if ( file.isBuffer() ) {
+      method = 'putBuffer';
+    } else if ( file.isStream() ) {
+      method = 'putStream';
+    }
+
+    if ( method ) {
+      client[method](file.contents, uploadPath, headers, function ( err, res ) {
+        if ( err || res.statusCode !== 200 ) {
+          gutil.log(gutil.colors.red('[FAILED]', file.path));
+        } else {
+          gutil.log(gutil.colors.green('[SUCCESS]', file.path));
+          res.resume();
+        }
+      });
+    } else {
+      throw PluginError(PLUGIN_NAME, 'File is neither Buffer or Stream!');
+    }
+
+    this.push(file);
+    return callback();
+  });
+
+  // returning the file stream
+  return stream;
+};
+
+// Exporting the plugin main function
+module.exports = gulpAwsS3;
